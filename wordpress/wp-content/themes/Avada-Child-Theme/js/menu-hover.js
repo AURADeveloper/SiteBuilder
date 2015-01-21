e4687d018aaba44cfa1c7b0f8966d28f46c163a2
Sam Edge <sam.edge@auraaccess.com.au>
2015-01-21 14:57:19 +1030

    Fixes positioning of the red line when sub menu items selected

--- wordpress/wp-content/themes/Avada-Child-Theme/js/menu-hover.js
+++ wordpress/wp-content/themes/Avada-Child-Theme/js/menu-hover.js
@@ -12,6 +12,11 @@ function shiftLine(s) {
         return false;
     }
 
+    // ignore submenu items - we only want the red line positioned on parent elements
+    if (jQuery(s).hasClass("fusion-dropdown-submenu")) {
+        return false;
+    }
+
     // pre-conditions passed: shift the line
     var position = jQuery(s).position();
     var left = position.left;
@@ -25,16 +30,16 @@ jQuery(document).ready(
     function() {
         var bottomBarElement = '<div class="avada-row"><div class="bottom-bar"></div></div>';
         jQuery("#small-nav").append(bottomBarElement);
-        if (shiftLine('.current_page_item') == false) {
-            shiftLine('.current-page-ancestor');
+        if (shiftLine('.current-menu-parent') == false) {
+            shiftLine('.current-menu-item');
         }
 
         jQuery("#nav ul > li").hover(
             function() {
                 shiftLine(this);
             }, function() {
-                if (shiftLine('.current_page_item') == false) {
-                    shiftLine('.current-page-ancestor');
+                if (shiftLine('.current-menu-parent') == false) {
+                    shiftLine('.current-menu-item');
                 }
             }
         );
