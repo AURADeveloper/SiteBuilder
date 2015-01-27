6f7a5a5c6e2dd06415cec84a5e834313fc8b0c91
Sam Edge <sam.edge@auraaccess.com.au>
2015-01-27 13:08:00 +1030

    Adds recaptcha plugin

---
+++ wordpress/wp-content/plugins/wp-recaptcha-integration/js/recaptcha-options.js
@@ -0,0 +1,39 @@
+(function($){
+	$(document).ready(function(){
+		$('[name="recaptcha_flavor"]:checked').trigger('click');
+	});
+	$(document).on('click','[name="recaptcha_flavor"]',function(){
+		$('.recaptcha-select-theme')
+			.removeClass('flavor-recaptcha')
+			.removeClass('flavor-grecaptcha')
+			.addClass('flavor-'+$(this).val());
+	});
+	
+	
+	$(document).on('click','#test-api-key' , function(e){
+		if ( ! $('#recaptcha-test-result').length )
+			$(this).closest('div').append('<div id="recaptcha-test-result" />');
+		$('#recaptcha-test-result').load( $(this).prop('href') );
+		e.preventDefault();
+		e.stopPropagation();
+		$(this).remove();
+		return false;
+	});
+	$(document).on('click','#recaptcha-test-verification' , function(e){
+		var data = {
+			'action' : 'recaptcha-test-verification',
+			'_wpnonce' : $('[name="recaptcha-test-verification-nonce"]').val(),
+			'g-recaptcha-response' : $('#g-recaptcha-response').val()
+		};
+		
+		$.post( ajaxurl , 
+			data , 
+			function(response) {
+				$('#recaptcha-test-result').html(response);
+			});
+		
+		e.preventDefault();
+		e.stopPropagation();
+		return false;
+	});
+})(jQuery);
